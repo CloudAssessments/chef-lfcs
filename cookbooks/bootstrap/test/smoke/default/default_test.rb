28b593bca254f3b5d75767fd48d8064ddef9f98d
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-08-09 15:37:05 -0500

    add tests

--- cookbooks/bootstrap/test/smoke/default/default_test.rb
+++ cookbooks/bootstrap/test/smoke/default/default_test.rb
@@ -7,12 +7,31 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
+    it { should exist }
+  end
+
+  describe user('cloud_user') do
     it { should exist }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file('/etc/ssh/sshd_config') do
+  its(:content) { should contain 'PasswordAuthentication yes' }
+end
+
+describe file('/opt/scripts/fix-reorts.sh') do
+  it { should exist }
+end
+
+describe file('/home/cloud_user/irs-records.txt') do
+  it { should exist }
+end
+
+describe file('/home/cloud_user/private-records.txt') do
+  it { should exist }
+end
+
+describe file('/home/cloud_user/reboot.bad') do
+  it { should exist }
 end
