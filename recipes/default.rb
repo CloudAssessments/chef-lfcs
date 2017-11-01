ba3730c4e23e7b081a203c4266bf1db5c3490fb8
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-11-01 10:54:14 -0500

    remove bootstraping code

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,42 +4,6 @@
 #
 # Copyright:: 2017, Travis N. Thomsen for Linux Academy, All Rights Reserved.
 
-include_recipe 'sshd::default'
-
-service 'sshd' do
-  action :stop
-end
-
-if node['platform_family'] == "debian"
-
-  user 'cloud_user' do
-    home '/home/cloud_user'
-    manage_home true
-    shell '/bin/bash'
-    password '$1$linuxaca$iGMxZ4g4lbPmfEDPhW3lw1'
-    salt 'linuxacademy'
-    gid 'sudo'
-  end
-
-  group 'cloud_user' do
-    members 'cloud_user'
-  end
-
-  execute 'add cloud_user to sudoers' do
-    command '/bin/echo \'cloud_user ALL=(ALL:ALL) NOPASSWD: ALL\' >> /etc/sudoers'
-  end
-
-  openssh_server '/etc/ssh/sshd_config' do
-    PasswordAuthentication yes
-  end
-end
-
-if node['platform_family'] == "rhel"
-  execute 'add cloud_user to sudoers' do
-    command '/bin/echo \'cloud_user  ALL=(ALL)  NOPASSWD: ALL\' >> /etc/sudoers'
-  end
-end
-
 directory '/opt/scripts' do
   owner 'root'
   group 'root'
@@ -77,8 +41,4 @@ remote_file '/home/cloud_user/reboot.bad' do
   mode '644'
   source 'https://raw.githubusercontent.com/CloudAssessments/lfcsa-lab-files/master/reboot.bad'
   action :create
-end
-
-service 'sshd' do
-  action :start
 end
